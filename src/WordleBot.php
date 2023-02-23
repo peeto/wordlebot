90db071f154fb28885e3aca72b71201933d5184a
peeto <chris@peeto.net>
2023-02-23 12:27:52 +1100

    Check parameters exist

--- src/WordleBot.php
+++ src/WordleBot.php
@@ -17,10 +17,12 @@ class WordleBot {
     }
     
     public function search( $data = []) {
-        foreach ($data['letters'] as $param) {
+        if (isset($data['letters'])) foreach ($data['letters'] as $param) {
             $this->db->addSearchParameter($param['state'], $param['letter'], $param['position']);
         }
-        return $this->db->doSearch($data['sort']=='1');
+        return $this->db->doSearch(
+            isset($data['sort']) && $data['sort']=='1'
+        );
     }
     
     public function render(array $options = []) {
